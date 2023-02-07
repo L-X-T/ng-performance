bbb49a53f4e04b236d69ee8e9fb57acdcf5c0f9c
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-02-07 12:32:19 +0100

    Dynamical lazy loading one chart without the router

--- src/app/flight-booking/charts/charts.component.ts
+++ src/app/flight-booking/charts/charts.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.css']
 })
-export class ChartsComponent {
+export class ChartsComponent implements AfterViewInit {
   title = 'Charts';
   chartsCount = 4;
   charts: { id: number; data: string }[] = [];
+
+  @ViewChild('cnt', { read: ViewContainerRef }) vC!: ViewContainerRef;
 
   constructor() {
     for (let index = 1; index <= this.chartsCount; index++) {
@@ -18,5 +20,12 @@ export class ChartsComponent {
       }
       this.charts.push({ id: index, data: 'data' + dataNumber });
     }
+  }
+
+  async ngAfterViewInit() {
+    const esm = await import('../chart/chart.component');
+    const chartRef = this.vC.createComponent(esm.ChartComponent);
+    chartRef.instance.id = this.charts[0].id;
+    chartRef.instance.data = this.charts[0].data;
   }
 }
