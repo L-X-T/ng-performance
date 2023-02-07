231ae165aec1c9f7b788783ef006e778bcedf7e2
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-02-07 12:43:45 +0100

    Dynamical lazy loading all charts without the router

--- src/app/flight-booking/charts/charts.component.ts
+++ src/app/flight-booking/charts/charts.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, QueryList, ViewChildren, ViewContainerRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChartComponent } from '../chart/chart.component';
@@ -15,7 +15,7 @@ export class ChartsComponent implements AfterViewInit {
   chartsCount = 4;
   charts: { id: number; data: string }[] = [];
 
-  @ViewChild('cnt', { read: ViewContainerRef }) vC!: ViewContainerRef;
+  @ViewChildren('cnt', { read: ViewContainerRef }) vCs!: QueryList<ViewContainerRef>;
 
   constructor() {
     for (let index = 1; index <= this.chartsCount; index++) {
@@ -29,8 +29,14 @@ export class ChartsComponent implements AfterViewInit {
 
   async ngAfterViewInit() {
     const esm = await import('../chart/chart.component');
-    const chartRef = this.vC.createComponent(esm.ChartComponent);
+    /*const chartRef = this.vCs.createComponent(esm.ChartComponent);
     chartRef.instance.id = this.charts[0].id;
-    chartRef.instance.data = this.charts[0].data;
+    chartRef.instance.data = this.charts[0].data;*/
+
+    this.vCs.map((viewContainerRef: ViewContainerRef, index: number) => {
+      const chartRef = viewContainerRef.createComponent(esm.ChartComponent);
+      chartRef.instance.id = this.charts[index].id;
+      chartRef.instance.data = this.charts[index].data;
+    });
   }
 }
